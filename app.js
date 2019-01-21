87dc2a7ffbb7ffd3205567b6f1b2d5eb4b7d05f3
jose rodolfo perez <thejoserodolfo@gmail.com>
2019-01-21 00:44:21 -0800

    handling different routes with expressjs

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
-// require http from node core modules
-const http = require('http');
 const express = require('express')
 
 // initializes a new express object behind the scenes where it manages and stores logic for us
 const app = express()
 
-app.use((req, res, next )=>{
-  console.log('in the middleware')
-  res.send('<h1> hello from express </h1>')
-  // next() //we should call "next()" if we dont send a response, otherwise the request will die here and not continue to the next middlware
+// request goes through files from top to bottom
+app.use('/product', (req, res, next )=>{
+  console.log('T')
+  res.send('<h1> This is our product page </h1>')
 })
 
-app.use((req, res, next )=>{
-  console.log('second middleware')
+app.use('/', (req, res, next )=>{
+  console.log('T')
+  res.send('<h1> This is our home path </h1>')
 })
 
-// app.listen(), replaces all code commented below it, note there is no need to require http neither.
+
+
 app.listen(5000)
-
-
-// // select a port of choice
-// const port = 5000;
-//
-// // set up of server
-// const server = http.createServer (app);
-//
-// // listens for server, logs when server is running
-// server.listen(port, () =>{
-//   console.log('server is running on port 5000')
-// })
