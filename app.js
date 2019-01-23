6e5fe7f91503d13b8e1f8b28706c0c93cd228556
jose rodolfo perez <thejoserodolfo@gmail.com>
2019-01-23 15:15:06 -0800

    serving up html files to my routes

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express();
 
+
 // parses all requst/respones below
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public'))) //this forwards us to any html file making a css request in their 'link' tag. 
 
 app.use('/admin', adminRoutes)
 
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
-
 
 app.listen(5000)
--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const path = require('path')
 
 
-
-router.use('/', (req,res,next) =>{
+// using get/post method exercises exact match for '/'
+router.get('/', (req,res,next) =>{
   res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')) //two underscores
 })
 
