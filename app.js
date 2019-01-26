f21cababac5e919c59debedf56d165f82bb5e829
jose rodolfo perez <thejoserodolfo@gmail.com>
2019-01-25 23:27:17 -0800

    installing and implementing pug

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const shopRoutes = require('./routes/shop')
 
 const app = express();
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 
 // parses all requst/respones below
 app.use(bodyParser.urlencoded({extended: false}));
--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,7 @@ const adminData = require('./admin')
 
 // using get/post method exercises exact match for '/'
 router.get('/', (req,res,next) =>{
-  console.log(adminData.products)
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')) //two underscores
+  res.render('shop')
 })
 
 
